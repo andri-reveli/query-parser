d1912814d44c50e5466dbceb4ba7d441bc6057dd
Andri Reveli <reveliandri@gmail.com>
2021-05-03 10:40:49 +0200

    improved & operation

--- src/main/java/AndQueryOperation.java
+++ src/main/java/AndQueryOperation.java
@@ -1,13 +1,17 @@
+import java.util.HashSet;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class AndQueryOperation implements QueryOperation {
     @Override
     public Query query(Query first, Query second) {
+        HashSet<List<String>> set1 = new HashSet<>(first.database());
+        HashSet<List<String>> set2 = new HashSet<>(second.database());
+
         return new Query(
-                first.database()
-                     .stream()
-                     .filter(list -> second.database().contains(list))
-                     .collect(Collectors.toList())
+                set1.stream()
+                    .filter(set2::contains)
+                    .collect(Collectors.toList())
         );
     }
 }
