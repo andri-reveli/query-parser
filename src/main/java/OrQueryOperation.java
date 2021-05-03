9bcfb71ef9a73e071c3d7d7ca14868093ca5ff4a
Andri Reveli <reveliandri@gmail.com>
2021-05-03 10:46:25 +0200

    improved | operation

--- src/main/java/OrQueryOperation.java
+++ src/main/java/OrQueryOperation.java
@@ -1,21 +1,18 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class OrQueryOperation implements QueryOperation {
     @Override
     public Query query(Query first, Query second) {
-        List<List<String>> union = new ArrayList<>();
+        HashSet<List<String>> set1 = new HashSet<>(first.database());
+        HashSet<List<String>> set2 = new HashSet<>(second.database());
 
-        first.database().forEach(list -> {
-            if (!union.contains(list)) {
-                union.add(list);
-            }
-        });
-        second.database().forEach(list -> {
-            if (!union.contains(list)) {
-                union.add(list);
-            }
-        });
-        return new Query(union);
+        return new Query(
+                Stream.concat(set1.stream(), set2.stream())
+                      .distinct()
+                      .collect(Collectors.toList())
+        );
     }
 }
